6f716d8c1c2132570ebed2efc043db2ccfd67573
CosminPerRam <cosmin.p@live.com>
2023-06-27 16:51:46 +0300

    Show only messages from an hour ago

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @messages = Message.all
+    @an_hour_ago = (Time.now.utc - 1*60*60).strftime("%d/%m/%Y %H:%M:%S %Z")
+
+    @messages = Message.where(created_at: @an_hour_ago..)
     @likes = Like.all
   end
 end
