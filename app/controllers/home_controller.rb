6b690b14038e8533c7d951d8fb0e8103273bc9bd
CosminPerRam <cosmin.p@live.com>
2023-06-27 18:49:32 +0300

    Show likes per message and show button for like/unlike

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,5 @@ class HomeController < ApplicationController
     @an_hour_ago = (Time.now.utc - 1*60*60).strftime("%d/%m/%Y %H:%M:%S %Z")
 
     @messages = Message.where(created_at: @an_hour_ago..)
-    @likes = Like.all
   end
 end
