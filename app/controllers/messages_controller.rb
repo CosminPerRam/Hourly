1dc6931782230fc1be0a3708ed06f6d00dccd34c
CosminPerRam <cosmin.p@live.com>
2023-06-27 18:31:56 +0300

    Dont let user create new message if one is already made

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   before_action :set_message, only: %i[ mine show edit update destroy ]
   before_action :authenticate_user!, except: %i[ show ]
+  before_action :has_already_made_in_hour
   before_action :correct_user, only: %i[ edit update destroy ]
 
   # GET /messages/1 or /messages/1.json
@@ -15,6 +16,12 @@ class MessagesController < ApplicationController
   def correct_user
     @owner = current_user.messages.find_by(id: params[:id])
     redirect_to messages_path, notice: "cacat" if @message.nil?
+  end
+
+  def has_already_made_in_hour
+    an_hour_ago = (Time.now.utc - 1*60*60).strftime("%d/%m/%Y %H:%M:%S %Z")
+    messages = Message.find_by(user_id: current_user.id, created_at: an_hour_ago..)
+    @has_already_made_in_hour = !messages.nil?
   end
 
   # GET /messages/new
